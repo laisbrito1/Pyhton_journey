67f1070409ddff0d9b01567872f55de5b2fa6381
Lais Brito <65972314+laisbrito1@users.noreply.github.com>
2023-08-07 17:42:44 -0400

    Add files via upload

---
+++ exercicioPass.py
@@ -0,0 +1,12 @@
+
+
+for num in range(1, 11):
+    if num % 2 == 0:
+        pass
+    else:
+        print(num)
+print('Laço encerrado')
+
+#A instrução pass atua sobre a estrutura if,
+#  permitindo que ela seja escrita sem outras instruções a serem executadas caso a condição seja verdadeira. 
+# Assim, podemos concentrar as instruções no caso em que a condição é falsa.
